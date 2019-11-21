46674615ba180d606561d5a4897ca14acc4c223a
Guru 2.0 <nikolay.staykov98@abv.bg>
2019-11-21 12:30:27 +0200

    Main Menu Scene

---
+++ New Unity Project/Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenuManager : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    public void CloseApplication()
+    {
+        Application.Quit();
+    }
+
+    public void LoadPvPScene()
+    {
+        SceneManager.LoadScene(1, LoadSceneMode.Single);
+    }
+
+    public void LoadPvEScene()
+    {
+        SceneManager.LoadScene(2, LoadSceneMode.Single);
+    }
+}
